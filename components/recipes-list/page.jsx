ccb4b4d5a1fbd9b765bc31c3814bc2ebaa2882b5
Laxman67 <laxmanhenricks94@gmail.com>
2024-12-20 13:09:16 +0530

    recipe list

--- components/recipes-list/page.jsx
+++ components/recipes-list/page.jsx
@@ -11,18 +11,35 @@ import Link from 'next/link';
 
 const RecipeList = ({ recipeList }) => {
   return (
-    <div className="p-4 mx-auto lg:max-w-lg  md:max-w-4xl sm:max-w-full">
+    <div className="p-4 mx-auto lg:w-[98%]  md:max-w-4xl sm:max-w-full">
       <div className="text-4xl font-bold text-gray-800 mb-12">
         <>Recipes</>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-3 lg:grid-cols-3 lg:gap-9 lg:p-4 ">
         {recipeList && recipeList.length > 0
           ? recipeList.map((recipe) => {
               return (
-                <Link href="/">
+                <Link href={`recipe-list/${recipe.id}`}>
                   <Card>
-                    <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1] transition-all">
-                      <div></div>
+                    <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.07] transition-all">
+                      <div className="w-full aspect-h-8 lg:h-80 ">
+                        <img
+                          src={recipe.image}
+                          alt={recipe.name}
+                          className="h-full w-full object-cover object-top"
+                        />
+                      </div>
+                      <CardDescription>
+                        <strong>Name:{recipe.name}</strong>
+                      </CardDescription>
+                      <div className="flex justify-between">
+                        <p>
+                          <b>Rating</b>: {recipe.rating}
+                        </p>
+                        <strong className="text-gray-700">
+                          {recipe.cuisine}
+                        </strong>
+                      </div>
                     </CardContent>
                   </Card>
                 </Link>
